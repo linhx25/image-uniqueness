6f864931fdc2afa2e35866bb1e692025ce2d1124
linhx25 <304455073@qq.com>
2022-06-07 20:50:05 -0400

    add scene classification

--- scene_clf.py
+++ scene_clf.py
@@ -1,19 +1,21 @@
-# PlacesCNN to predict the scene category, attribute, and class activation map in a single pass
-# by Bolei Zhou, sep 2, 2017
-# updated, making it compatible to pytorch 1.x in a hacky way
-
-import torch
-from torch.autograd import Variable as V
-import torchvision.models as models
-from torchvision import transforms as trn
-from torch.nn import functional as F
 import os
 import numpy as np
+import pandas as pd
+import torch
+import torchvision
+from torch.nn import functional as F
+from operator import itemgetter
+import sys; sys.path.append("../")
+import src.dataset
+
 import cv2
-from PIL import Image
+from tqdm import tqdm
+from torch.utils.data import DataLoader
 
+### The GPU output is slightly different from CPU. I don't konw why.
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
- # hacky way to deal with the Pytorch 1.0 update
+# hacky way to deal with the Pytorch 1.0 update
 def recursion_change_bn(module):
     if isinstance(module, torch.nn.BatchNorm2d):
         module.track_running_stats = 1
@@ -64,10 +66,12 @@ def load_labels():
 
     return classes, labels_IO, labels_attribute, W_attribute
 
+
 def hook_feature(module, input, output):
     global features_blobs
     features_blobs.append(np.squeeze(output.data.cpu().numpy()))
 
+    
 def returnCAM(feature_conv, weight_softmax, class_idx):
     # generate the class activation maps upsample to 256x256
     size_upsample = (256, 256)
@@ -81,15 +85,6 @@ def returnCAM(feature_conv, weight_softmax, class_idx):
         cam_img = np.uint8(255 * cam_img)
         output_cam.append(cv2.resize(cam_img, size_upsample))
     return output_cam
-
-def returnTF():
-# load the image transformer
-    tf = trn.Compose([
-        trn.Resize((224,224)),
-        trn.ToTensor(),
-        trn.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
-    return tf
 
 
 def load_model():
@@ -110,83 +105,78 @@ def load_model():
     for i, (name, module) in enumerate(model._modules.items()):
         module = recursion_change_bn(model)
     model.avgpool = torch.nn.AvgPool2d(kernel_size=14, stride=1, padding=0)
-    
-    model.eval()
-
-    # the following is deprecated, everything is migrated to python36
-
-    ## if you encounter the UnicodeDecodeError when use python3 to load the model, add the following line will fix it. Thanks to @soravux
-    #from functools import partial
-    #import pickle
-    #pickle.load = partial(pickle.load, encoding="latin1")
-    #pickle.Unpickler = partial(pickle.Unpickler, encoding="latin1")
-    #model = torch.load(model_file, map_location=lambda storage, loc: storage, pickle_module=pickle)
 
     model.eval()
     # hook the feature extractor
-    features_names = ['layer4','avgpool'] # this is the last conv layer of the resnet
+    features_names = ['layer4'] # this is the last conv layer of the resnet
     for name in features_names:
         model._modules.get(name).register_forward_hook(hook_feature)
+    
     return model
 
+
 if __name__ == "__main__":
-
-    # load the labels
+    inpath = "/export/projects2/szhang_text_project/Airbnb_unique/photo_library/photo_library_batch0.h5"
+    score_path="/export/home/rcsguest/rcs_hlin/Airbnb_unique/image-uniqueness/output/05-26_15:32:10/train_loss.pkl"
+    
+    ## dataset
+    ds = src.dataset.AirbnbDataset(inpath)
+    loader = DataLoader(ds, batch_size=64, drop_last=False)
+    mapping = ds.idx_mapping.reset_index().set_index("image_id")
+    
+    ## uniqueness
+    uniqueness = pd.read_pickle(score_path)
+    
+    ## labels
     classes, labels_IO, labels_attribute, W_attribute = load_labels()
-
-    # load the model
+    
+    ## model
     features_blobs = []
     model = load_model()
+    model.to(device)
+    
+    ## transform
+    tsfm = torchvision.transforms.Compose([
+        torchvision.transforms.Resize((224,224)),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+    loader.dataset.transform = tsfm
 
-    # load the transformer
-    tf = returnTF() # image transformer
-
-    # get the softmax weight
+    ## get the softmax weight
     params = list(model.parameters())
-    weight_softmax = params[-2].data.numpy()
-    weight_softmax[weight_softmax<0] = 0
+    weight_softmax = params[-2].data.cpu().numpy()
+    weight_softmax[weight_softmax < 0] = 0
 
-    # load the test image
-    img_url = 'http://places.csail.mit.edu/demo/6.jpg'
-    os.system('wget %s -q -O test.jpg' % img_url)
-    img = Image.open('test.jpg')
-    input_img = V(tf(img).unsqueeze(0))
+    preds = []
+    for batch in tqdm(loader):
+        images, ids = batch["image"], batch["label"]
+        
+        ## prediction
+        with torch.no_grad():
+            logit = model(images.to(device))
+            h_x = F.softmax(logit, 1).data.squeeze()
+            prob, idx = h_x.sort(-1, True)
+            prob = prob.cpu().numpy()[:, 0]
+            idx = idx.cpu().numpy()
 
-    # forward pass
-    logit = model.forward(input_img)
-    h_x = F.softmax(logit, 1).data.squeeze()
-    probs, idx = h_x.sort(0, True)
-    probs = probs.numpy()
-    idx = idx.numpy()
+        ## output the IO prediction
+        io_pred = labels_IO[idx[:,:10]].mean(axis=-1) # vote for the indoor or outdoor
+        io_pred = (io_pred > 0.5).astype(int)
 
-    print('RESULT ON ' + img_url)
+        ## output the prediction of scene category
+        cate_pred = itemgetter(*idx[:, 0])(classes)
 
-    # output the IO prediction
-    io_image = np.mean(labels_IO[idx[:10]]) # vote for the indoor or outdoor
-    if io_image < 0.5:
-        print('--TYPE OF ENVIRONMENT: indoor')
-    else:
-        print('--TYPE OF ENVIRONMENT: outdoor')
+        ## for our forcast
+        preds.append(pd.DataFrame({
+            "in/out": io_pred, 
+            "scene": cate_pred,
+            "prob": prob,
+        }, index=ids))
 
-    # output the prediction of scene category
-    print('--SCENE CATEGORIES:')
-    for i in range(0, 5):
-        print('{:.3f} -> {}'.format(probs[i], classes[idx[i]]))
+    
+    preds = pd.concat(preds).reindex(uniqueness.index)
+    uniqueness[["in/out", "scene", "prob"]] = preds
+    uniqueness.to_pickle("/export/home/rcsguest/rcs_hlin/Airbnb_unique/image-uniqueness/output/05-26_15:32:10/uniqueness.pkl")
 
-    # output the scene attributes
-    responses_attribute = W_attribute.dot(features_blobs[1])
-    idx_a = np.argsort(responses_attribute)
-    print('--SCENE ATTRIBUTES:')
-    print(', '.join([labels_attribute[idx_a[i]] for i in range(-1,-10,-1)]))
-
-
-    # generate class activation mapping
-    print('Class activation map is saved as cam.jpg')
-    CAMs = returnCAM(features_blobs[0], weight_softmax, [idx[0]])
-
-    # render the CAM and output
-    img = cv2.imread('test.jpg')
-    height, width, _ = img.shape
-    heatmap = cv2.applyColorMap(cv2.resize(CAMs[0],(width, height)), cv2.COLORMAP_JET)
-    result = heatmap * 0.4 + img * 0.5
-    cv2.imwrite('cam.jpg', result)
+        
